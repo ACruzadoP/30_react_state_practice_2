be2ca5d294d2ab675a484c560fdbc6a08c6ee075
Alberto <albertocruzadopleite@gmail.com>
2020-10-08 04:58:01 +0200

    Tomando snapshot de un ejercicio terminado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isLoggedIn: false
+        }
+    }
+
+    determineIfLogged() {
+        if (this.state.isLoggedIn) {
+            return "in"
+        } else {
+            return "out"
+        }
+    }
+
+    render () {
+        let isLogged = this.determineIfLogged()
+        return (
+            <div>
+                <h1>You are currently logged {isLogged}</h1>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
